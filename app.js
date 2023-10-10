64efa5932cfc741e175d97dd56060cb389bb126d
SaiKumar <bsainaidu90@gmail.com>
2023-10-10 19:06:06 +0530

    Updated fav icon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function startScrolling() {
     // adding text
     let scrollingText = document.createElement("div");
     scrollingText.className = "scrolling-text";
-    scrollingText.innerText = "Welcome to ClicKRR";
+    scrollingText.innerText = "Welcome to ClicKRRR";
 
     // adding text to button
     document.querySelector(".button-container").appendChild(scrollingText);
