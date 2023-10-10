f2e24ce263247dd0bd8863f4bd4d4bc75e8f39d8
SaiKumar <bsainaidu90@gmail.com>
2023-10-10 18:28:19 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// function to start the scrolling text
+function startScrolling() {
+    // adding text
+    let scrollingText = document.createElement("div");
+    scrollingText.className = "scrolling-text";
+    scrollingText.innerText = "Welcome to ClicKRR";
+
+    // adding text to button
+    document.querySelector(".button-container").appendChild(scrollingText);
+
+    
+}
+
+// trigger scrolling text when button cliked
+document.querySelector(".button").addEventListener("click", startScrolling);
